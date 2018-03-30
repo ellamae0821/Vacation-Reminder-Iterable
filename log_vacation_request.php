26358880070be8b52e542844b6fdd6563ae7ef2e
Ella Arana <ellamae207@gmail.com>
2018-03-30 09:58:27 -1000

    functions refactored to take arrays

--- log_vacation_request.php
+++ log_vacation_request.php
@@ -1,31 +1,83 @@
 <?php
 
-function vacation_request_log()
-	{
-		ini_set( 'display_errors', 1 );
-		ini_set( 'display_startup_errors', 1 );
-		error_reporting( E_ALL );
+
+// function for global.resources.php
+
+/**
+ * vacation_request_log function logs the user's vacation request activity to the tbl_vacation_request database
+ * @param  array customer_info
+ * @return obj updates the database
+ */
+
+
+function vacation_request_log($arr)
+{
+
+	$sql = "INSERT INTO tbl_vacation_request_log ";
+	$sql .= "(chr_email, chr_first_name, chr_last_name, dte_stop_date,";  
+	$sql .= " dte_restart_date, dte_created, dte_updated)";
+	$sql .= " VALUES ('";
+	$sql .= str_replace("'","\'",$arr["circProNameId"]) . "','";
+	$sql .= str_replace("'","\'",$arr["email"]) . "','";
+	$sql .= str_replace("'","\'",$arr["fname"]) . "','";
+	$sql .= str_replace("'","\'",$arr["lname"]) . "','";
+	$sql .= $arr["stop_date"] . "','";
+	$sql .= $arr["restart_date"] . "','";
+	$sql .= date("Y-m-d H:i:s"). "','";
+	$sql .= date("Y-m-d H:i:s") . "')'";
+	
+	mysql_query($sql) or die();
+
+}
+
+
+
+
+// $customer_info = array();
+// $customer_info['circProNameId'] = '123456';
+// $customer_info['email'] = 'araname22@gmail.com';
+// $customer_info['fname'] = 'na';
+// $customer_info['lname'] = 'ara';
+// $customer_info['stop_date'] = '2018-04-02';
+// $customer_info['restart_date'] = '2018-04-10';
+
+
+// 	function vacation_request_log($arr)
+// 	{
+// 		ini_set( 'display_errors', 1 );
+// 		ini_set( 'display_startup_errors', 1 );
+// 		error_reporting( E_ALL );
 		
-		$dbhost = 'localhost';
-	    $dbuser = 'online_ella';
-	    $dbpass = 'Hfu5py6Ku7T9UJmd';
-	    $db = 'dev';
-		$db_conn = mysql_connect( $dbhost, $dbuser, $dbpass );
+// 		$dbhost = 'localhost';
+// 		$dbuser = 'online_ella';
+// 		$dbpass = '-';
+// 		$db = 'dev';
+// 		$db_conn = mysql_connect( $dbhost, $dbuser, $dbpass );
+	
 
-		var_dump( $db_conn );
-		var_dump( mysql_select_db( $db ) );
+// 		var_dump( $db_conn );
+// 		var_dump( mysql_select_db( $db ) );
 
-		$sql = "INSERT INTO `tbl_vacation_request_log` (num_id, num_subscriber_id, chr_email, ";  
-		$sql .= "chr_first_name, chr_last_name, dte_stop_date, dte_restart_date,";
-		$sql .= "dte_created, dte_updated)";
-		$sql .= " VALUES (NULL, '1234567', 'earana@staradvetiser.com', 'newtest', 'arana', '2018-03-08', "; 
-		$sql .= " '2018-04-13', CURRENT_TIMESTAMP, '0000-00-00 00:00:00')" ;
+// 		// $sql = "INSERT INTO tbl_vacation_request_log ";
+// 		// $sql .= "(chr_email, ";  
+// 		// $sql .= "chr_first_name, chr_last_name, dte_stop_date, dte_restart_date,";
+// 		// $sql .= "dte_created, dte_updated)";
+// 		// $sql .= " VALUES ('";
+// 		// $sql .= str_replace("'","\'",$customer_info["circProNameId"]) . "','";
+// 		// $sql .= $customer_info["email"] . "','";
+// 		// $sql .= $customer_info["fname"] . "','";
+// 		// $sql .= $customer_info["lname"] . "','";
+// 		// $sql .= $customer_info["stop_date"] . "','";
+// 		// $sql .= $customer_info["restart_date"] . "','";
+// 		// $sql .= $customer_info["restart_date"] . "','";// for now, while working on how to insert current timestamp
+// 		// $sql .= $customer_info["restart_date"] . "')'";// for now, while working on how to insert current timestamp
 		
-        $result = mysql_query($sql);
+// 		$result = mysql_query($sql);
 
-        echo "New Vacation request logged";
+//         // Closing connection
+// 		mysql_close($db_conn);
 
-	}
+// 	}
 
-	vacation_request_log();
+
 ?>
--- update_user_vacation_dates.php
+++ update_user_vacation_dates.php
@@ -1,49 +1,55 @@
 <?php
-	ini_set( 'display_errors', 1 );
-	ini_set( 'display_startup_errors', 1 );
-	error_reporting( E_ALL );
 
-	require_once('Email_Service.php');
-	$iterable = new Email_Service('-', true, true);
 
-	function humanize($data){
-		echo "<pre>";
-		print_r($data);
-		echo "<pre>";
-	}
-
-	$email = 'ella-';
-	$publicationId = '1000016';
-	$circProNameId = '1581412';
-	$stop_date = "2018-04-01";
-	$restart_date = "2018-04-02";
 
 /**
- * update_user_vacation_dates function will add/update a user's field on iterable adding the stop_date & restart date 
- * @param  [type]
+ * update_user_vacation_dates function will update a user's info on iterable with a stop_date & restart_date 
  * @param  new Email Service 
- * @param  string 	$email
- * @param  string 	$publicationId
- * @param  string 	$circProNameId
- * @param  string 	$stop_date
- * @param  string 	$restart_date
+ * @param  array 	contains email, publicationId, circProNameId, stop_date and restart_date
  */
 
-	function update_user_vacation_dates($iterable, $email, $publicationId, $circProNameId, $stop_date, $restart_date ){
-		$user = $iterable->user_get_by_email($email);
-		// humanize($user);
+
+//function for Email Service 
+	function update_user_vacation_dates($arr){
+		$user = user_get_by_email($arr['email']);
 		$updatedSubscriptions = array();
 		foreach ($user['content']['user']['dataFields']['subscriptions'] as $key => $subscription) {
-			if( $subscription['publicationId'] == $publicationId && $subscription['circProNameId'] == $circProNameId)
+			if( $subscription['publicationId'] == $arr['publicationId'] && $subscription['circProNameId'] == $arr[circProNameId])
 			{
-				$subscription['stop_date'] = $stop_date;
-				$subscription['restart_date'] = $restart_date;
-				echo $key;
+				$subscription['stop_date'] = $arr['stop_date'];
+				$subscription['restart_date'] = $arr['restart_date'];
 			}
 			$updatedSubscriptions [] = $subscription;
 		}
-		$update = $iterable->user_update_by_email($email, array( "subscriptions" => $updatedSubscriptions ) );
+		$update = user_update_by_email($arr['email'], array( "subscriptions" => $updatedSubscriptions ) );
 	}
-	update_user_vacation_dates($iterable, $email, $publicationId, $circProNameId, $stop_date, $restart_date )
+
+
+
+
+//--------------------------------------------------------------------------------------------
+
+	// require_once('Email_Service.php');
+	// $iterable = new Email_Service('-', true, true);
+
+	// function humanize($data){
+	// 	echo "<pre>";
+	// 	print_r($data);
+	// 	echo "<pre>";
+	// }
+	
+	// function update_user_vacation_dates($iterable, $arr ){
+	// 	$user = $iterable->user_get_by_email($arr['email']);
+	// 	$updatedSubscriptions = array();
+	// 	foreach ($user['content']['user']['dataFields']['subscriptions'] as $key => $subscription) {
+	// 		if( $subscription['publicationId'] == $arr['publicationId'] && $subscription['circProNameId'] == $arr[circProNameId])
+	// 		{
+	// 			$subscription['stop_date'] = $arr['stop_date'];
+	// 			$subscription['restart_date'] = $arr['restart_date'];
+	// 		}
+	// 		$updatedSubscriptions [] = $subscription;
+	// 	}
+	// 	$update = $iterable->user_update_by_email($arr['email'], array( "subscriptions" => $updatedSubscriptions ) );
+	// }
 
 ?>
