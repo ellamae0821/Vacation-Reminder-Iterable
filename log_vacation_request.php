d3576253dc147f2a156bbe85e0cd2f7e72118ce5
Ella Arana <ellamae207@gmail.com>
2018-04-04 13:35:57 -1000

    updated table

--- log_vacation_request.php
+++ log_vacation_request.php
@@ -1,35 +1,34 @@
 <?php
-
-
-
 /*
  vacation_request_log function logs the user's vacation request activity to the tbl_vacation_request database
 */
 
-function vacation_request_log($circProNameId,$first_name,$last_name,$email,$stop_date,
-							  $restart_date, $num_subscriber_id, $num_publication_id, $chr_result
-							  )
-{
-	$dte_created = date('Y-m-d H:i:s');
 
-	$sql = "INSERT INTO tbl_vacation_request_log (num_name_id, chr_first_name,";
-	$sql .= "chr_last_name, chr_email, dte_stop_date, dte_restart_date, dte_created, ";
-	$sql .= "num_subscriber_id, num_publication_id, chr_result) ";
-	$sql .= "VALUES (";
-	$sql .= "'$circProNameId',";
-	$sql .= "'$first_name',";
-	$sql .= "'$last_name',";
-	$sql .= "'$email',";
-	$sql .= "'$stop_date',";
-	$sql .= "'$restart_date',";
-	$sql .= "'$dte_created',";
-	$sql .= "'$num_subscriber_id',";
-	$sql .= "'$num_publication_id',";
-	$sql .= "'$chr_result')";
+// function vacation_request_log($first_name,$last_name,$email,$stop_date,
+// 							  $restart_date, $name_id, $account_no, $publication_id
+// 							  )
+// {
+// 	$dte_created = date('Y-m-d H:i:s');
+	
+// 	$sql = "INSERT INTO tbl_vacation_request_log (chr_first_name,";
+// 	$sql .= "chr_last_name, chr_email, dte_stop_date, dte_restart_date, dte_created, ";
+// 	$sql .= "num_name_id, num_account_no, num_publication_id) ";
+// 	$sql .= "VALUES (";
+// 	$sql .= "'$first_name',";
+// 	$sql .= "'$last_name',";
+// 	$sql .= "'$email',";
+// 	$sql .= "'$stop_date',";
+// 	$sql .= "'$restart_date',";
+// 	$sql .= "'$dte_created',";
+// 	$sql .= "'$name_id',";
+// 	$sql .= "'$account_no,";
+// 	$sql .= "'$publication_id')";
 
 	
-	mysql_query($sql) or die();
-}
+// 	mysql_query($sql);
+// }
+
+
 
 //------------------------------------------------------------ TEST ------------------------------------------------------------
 $dbhost = 'localhost';
@@ -41,32 +40,30 @@ $db_conn = mysql_connect( $dbhost, $dbuser, $dbpass );
 var_dump( $db_conn );
 var_dump( mysql_select_db( $db ) );
 
-function vacation_request_log($circProNameId,$first_name,$last_name,$email,$stop_date,
-							  $restart_date, $num_subscriber_id, $num_publication_id, $chr_result
+function vacation_request_log($first_name,$last_name,$email,$stop_date,
+							  $restart_date, $name_id, $account_no, $publication_id
 							  )
 {
 	$dte_created = date('Y-m-d H:i:s');
 
-	$sql = "INSERT INTO tbl_vacation_request_log (num_name_id, chr_first_name,";
+	$sql = "INSERT INTO tbl_vacation_request_log (chr_first_name,";
 	$sql .= "chr_last_name, chr_email, dte_stop_date, dte_restart_date, dte_created, ";
-	$sql .= "num_subscriber_id, num_publication_id, chr_result) ";
+	$sql .= "num_name_id, num_account_no, num_publication_id) ";
 	$sql .= "VALUES (";
-	$sql .= "'$circProNameId',";
 	$sql .= "'$first_name',";
 	$sql .= "'$last_name',";
 	$sql .= "'$email',";
 	$sql .= "'$stop_date',";
 	$sql .= "'$restart_date',";
 	$sql .= "'$dte_created',";
-	$sql .= "'$num_subscriber_id',";
-	$sql .= "'$num_publication_id',";
-	$sql .= "'$chr_result')";
+	$sql .= "'$name_id',";
+	$sql .= "'$account_no,";
+	$sql .= "'$publication_id')";
 
-	
 	mysql_query($sql);
 }
 	
- vacation_request_log('1581412', 'whoami', 'arana', '-@gmail.com', '2018-04-04', '2018-04-10', 
- 	'2018-05-09', '123456', '1000016', 'awesomeness!');
+ vacation_request_log('Ella Mae', 'Arana', 'araname22@gmail.com', '2018-04-04', '2018-04-10', 
+ 	 '-', '-', '1000016');
 
 ?>
