fd2664d7181c3aa42f30e34ab54d25ba61b3e764
Ella Arana <ellamae207@gmail.com>
2018-04-05 15:16:51 -1000

    mysql_fetch_assoc issue resolved

--- new_crosschecker.php
+++ new_crosschecker.php
@@ -12,47 +12,53 @@
 	date_default_timezone_set("Pacific/Honolulu");
 	$date = date("Y-m-d");
 
-	$r = daily_vacation_request($date);
 
+	$r = daily_vacation_request($date);
+	print_pre($r);
 
 	foreach( $r as $key => $v_request)
 	{	
-		print_pre($v_request);
 		$circpro_vacation = vacationStatus($v_request['num_account_no']);
 		print_pre($circpro_vacation);
-
+	
 		if($circpro_vacation['stop-date'] == $v_request['dte_stop_date'])
 		{
-			update_log_result($v_request['num_account_no'], "success");
+			update_log_result("Success", $v_request['num_id']);
 		}else{
-			update_log_result($v_request['num_account_no'], "boooo");
+			update_log_result("Doughnut", $v_request['num_id']);//for testing purposes only. No else statement on actual function
 		}
 	}
 
+
+ 
 	function daily_vacation_request($date) 
 	{
 		$dbhost = 'localhost';
 		$dbuser = 'online_ella';
 		$dbpass = '-';
-		$db = 'dev';
+		$db = '-';
 		$db_conn = mysql_connect( $dbhost, $dbuser, $dbpass );
 		var_dump( $db_conn );
 		var_dump( mysql_select_db( $db ) );
 
-		$sql = "SELECT * FROM tbl_vacation_request_log WHERE Date(dte_created) ='$date'";
-		$result = mysql_query($sql);
-		$request_today = mysql_fetch_assoc($result);
-		$vacation = array();
+		$sql = "SELECT * FROM tbl_vacation_request_log WHERE Date(dte_created) ='$date' AND chr_result=''";
+		$sql_r = mysql_query($sql);
 
-	  	while($row = mysql_fetch_assoc($result)){
-		    $vacation [] = $row;
-		  }
-		// print_pre($vacation);
-		return $vacation;   
+		if ($sql_r)
+		{
+			$vacation = array() ;
+
+			while( $row = mysql_fetch_assoc($sql_r) )
+			{
+				$vacation [] = $row;
+			}
+			return $vacation;
+		}	
+
 	}
 
 	
-	function update_log_result($account_no, $status) 
+	function update_log_result($status, $num_id) 
 	{
 		$dbhost = 'localhost';
 		$dbuser = 'online_ella';
@@ -64,10 +70,9 @@
 
 		$sql = "UPDATE tbl_vacation_request_log 
 		SET chr_result = '$status'
-		WHERE num_account_no = '$account_no'";
+		WHERE num_id = '$num_id'";
 		$result = mysql_query($sql);
 		echo "Records updated successfully!" . ($sql);
-
 		return $result;   
 	}
 
